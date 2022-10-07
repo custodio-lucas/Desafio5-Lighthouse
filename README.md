6625defb538ba67328fb3e010d8f6977d8fda2f4
Lucas Marques <lucascmgs@gmail.com>
2022-10-07 19:03:18 -0300

    Entrega do Desafio5

---
+++ README.md
@@ -0,0 +1,47 @@
+# Desafio 5
+## Engenharia de dados e Airflow
+
+Este é um repositório feito para o desafio de engenharia de dados do programa Lighthouse da Indicium. O desafio visa a criação de duas tasks em uma DAG no Airflow: a primeira deve ler a tabela "Order" no banco de dados Northwind (disponível na pasta data) e gerar um documento "output_orders.csv", já a segunda deveria ler esse arquivo gerado pela primeira task, fazer um join com a tabela "OrderDetail" do banco de dados Northwind e gerar um documento "count.txt" contendo a soma da quantidade vendida para o Rio de Janeiro. Após isso, bastava criar uma variável específica no airflow para que a terceira task fosse executada e gerasse um output final.
+
+## Requisitos
+
+Todos pacotes e biblitecas necessários para a instalação estão no arquivo requirements.txt. Para instalá-los, basta seguir o passo a passo:
+
+1- Baixar o "virtualenv"
+```sh
+pip install virtualenv
+```
+2- Criar o ambiente virtual
+```sh
+virtualenv venv -p python3
+```
+3- Iniciar o ambiente
+```sh
+source venv/bin/activate
+```
+4- Instalar o conteúdo de requirements.txt
+```sh
+pip install -r /path/to/requirements.txt
+```
+sendo /path/to o caminho para o arquivo requirements.txt.
+
+## Execução do Airflow
+
+Para executar o airflow em seu ambiente, basta seguir o passo a passo:
+
+1- Criar o arquivo .env no seu projeto
+```sh
+cat > .env 
+```
+2- Criar a variável de ambiente nesse arquivo
+```sh
+touch "export AIRFLOW_HOME=/home/seu_usuario/.../seu_projeto/" > .env
+```
+3- Executar o .env
+```sh
+source .env
+```
+4- Por fim, basta executar o Airflow
+```sh
+airflow standalone
+```
---
+++ dags/example_desafio.py
@@ -0,0 +1,100 @@
+import sqlite3
+import pandas as pd
+from airflow.utils.edgemodifier import Label
+from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow import DAG
+from airflow.models import Variable
+
+db_path='/home/lucas/indicium/Desafio5/data/Northwind_small.sqlite'
+
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+def read_nw_orders():
+
+    conn = sqlite3.connect(db_path, isolation_level=None,
+                           detect_types=sqlite3.PARSE_COLNAMES)
+    df = pd.read_sql_query('select * from "Order"', conn)
+    df.to_csv('output_orders.csv', index=False)
+    
+    return None
+
+def read_nw_od():
+    
+    conn = sqlite3.connect(db_path, isolation_level=None,
+                           detect_types=sqlite3.PARSE_COLNAMES)
+    df_od = pd.read_sql_query('select * from "OrderDetail"', conn)
+    df_o = pd.read_csv('/home/lucas/indicium/Desafio5/output_orders.csv')
+
+    df = pd.merge(df_od, df_o, left_on='OrderId', right_on='Id', how='left')
+    
+    soma = df[df['ShipCity']=='Rio de Janeiro']['Quantity'].sum()
+
+    with open('count.txt', 'w+') as f:
+        f.write(str(soma))
+
+    return None
+
+## Do not change the code below this line ---------------------!!#
+def export_final_answer():
+    import base64
+
+    # Import count
+    with open('count.txt') as f:
+        count = f.readlines()[0]
+
+    my_email = Variable.get("my_email")
+    message = my_email+count
+    message_bytes = message.encode('ascii')
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode('ascii')
+
+    with open("final_output.txt","w") as f:
+        f.write(base64_message)
+    return None
+## Do not change the code above this line-----------------------##
+
+with DAG(
+    'DesafioAirflow',
+    default_args=default_args,
+    description='Desafio de Airflow da Indicium',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=['example'],
+) as dag:
+    dag.doc_md = """
+        Esse é o desafio de Airflow da Indicium.
+    """
+   
+    write_output_orders = PythonOperator(
+        task_id='write_output_orders',
+        python_callable=read_nw_orders,
+        provide_context=True
+    )
+
+    write_count = PythonOperator(
+    task_id='write_count',
+    python_callable=read_nw_od,
+    provide_context=True
+    )
+
+    export_final_output = PythonOperator(
+        task_id='export_final_output',
+        python_callable=export_final_answer,
+        provide_context=True
+    )
+
+    write_output_orders >> write_count >> export_final_output
